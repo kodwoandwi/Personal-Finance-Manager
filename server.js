f5cb85bffedd4e5ae275a84291800739de136356
Benjamin Afful <andwikodwo@outlook.com>
2024-12-08 02:47:11 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mong
